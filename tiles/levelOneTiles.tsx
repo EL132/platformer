52954ebfd8dc91e47b4a2bf3b7b082b647cb283f
EL132 <elias.lind6@gmail.com>
2023-02-10 14:17:50 -0500

    indented line in levelOnesTiles

--- tiles/levelOneTiles.tsx
+++ tiles/levelOneTiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="Level One Tiles - EL" tilewidth="32" tileheight="32" tilecount="200" columns="10">
- <image source="./images/small-edit.png" width="320" height="641"/>
+<image source="./images/small-edit.png" width="320" height="641"/>
 </tileset>
