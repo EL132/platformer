c6ba7058658c07741ad772fe266a4332ab56260b
handasahil <sahilhanda101@gmail.com>
2023-02-17 14:33:12 -0500

    added ninja files

---
+++ levelSelector/Custom/code/path.py
@@ -0,0 +1,16 @@
+import pygame 
+from pytmx.util_pygame import load_pygame
+
+pygame.init()
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 448
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+tmx_data = load_pygame('./maps/levelOne.tmx')
+
+for layer in tmx_data.visible_layers:
+    print(layer.name)
+
+class PathBorder(): 
+    pass
