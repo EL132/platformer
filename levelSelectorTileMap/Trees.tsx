cd98ad559c6b128b6dc41e200cd67d39187f0ed5
handasahil <113319786+handasahil@users.noreply.github.com>
2023-04-15 22:59:05 -0400

    creating new level selector map

---
+++ levelSelectorTileMap/Trees.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Trees" tilewidth="28" tileheight="44" tilecount="9" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="27" height="44" source="setOne/2 Objects/Trees/1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="23" height="38" source="setOne/2 Objects/Trees/2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="28" height="32" source="setOne/2 Objects/Trees/3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="27" height="39" source="setOne/2 Objects/Trees/4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="26" height="38" source="setOne/2 Objects/Trees/5.png"/>
+ </tile>
+ <tile id="5">
+  <image width="15" height="22" source="setOne/2 Objects/Trees/6.png"/>
+ </tile>
+ <tile id="6">
+  <image width="10" height="28" source="setOne/2 Objects/Trees/7.png"/>
+ </tile>
+ <tile id="7">
+  <image width="11" height="37" source="setOne/2 Objects/Trees/8.png"/>
+ </tile>
+ <tile id="8">
+  <image width="10" height="43" source="setOne/2 Objects/Trees/9.png"/>
+ </tile>
+</tileset>
