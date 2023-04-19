bf53d2ac4745c342590f88f5c4c42581a7698841
EL132 <elias.lind6@gmail.com>
2023-04-19 10:36:22 -0400

    began new forest tileset

--- Levels/LevelOne/levelOne.py
+++ Levels/LevelOne/levelOne.py
@@ -489,10 +489,6 @@ class LevelOne():
         sub_rect = sub_text.get_rect()
         sub_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2 + 64)
 
-        blurred_background = pygame.transform.box_blur(display_surface, 4)
-        pygame.image.save(blurred_background, "blurred.jpg")
-        blurred_rect = blurred_background.get_rect(topleft = (0, 0))
-        display_surface.blit(blurred_background, blurred_rect)
 
         #Display the pause text
         # display_surface.fill(BLACK)
