151e5a64df06b7cde569587b4327d92413a0f466
EL132 <elias.lind6@gmail.com>
2023-04-21 12:44:57 -0400

    file reorganization

---
+++ Levels/LevelTwo/tiles/Objects.tsx
@@ -0,0 +1,97 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Objects" tilewidth="160" tileheight="160" tilecount="31" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="../images/tileset/Objects/Cacti/5.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="../images/tileset/Objects/Cacti/6.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="../images/tileset/Objects/Cacti/7.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="../images/tileset/Objects/Cacti/8.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="../images/tileset/Objects/Cacti/9.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="../images/tileset/Objects/Cacti/1.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="../images/tileset/Objects/Cacti/2.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="../images/tileset/Objects/Cacti/3.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="../images/tileset/Objects/Cacti/4.png"/>
+ </tile>
+ <tile id="9">
+  <image width="160" height="128" source="../images/tileset/Objects/Pyramid/1.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="32" source="../images/tileset/Objects/Stones/5.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="32" source="../images/tileset/Objects/Stones/6.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="32" source="../images/tileset/Objects/Stones/7.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="32" source="../images/tileset/Objects/Stones/8.png"/>
+ </tile>
+ <tile id="14">
+  <image width="96" height="64" source="../images/tileset/Objects/Stones/9.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="../images/tileset/Objects/Stones/1.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="../images/tileset/Objects/Stones/2.png"/>
+ </tile>
+ <tile id="17">
+  <image width="32" height="32" source="../images/tileset/Objects/Stones/3.png"/>
+ </tile>
+ <tile id="18">
+  <image width="32" height="32" source="../images/tileset/Objects/Stones/4.png"/>
+ </tile>
+ <tile id="19">
+  <image width="97" height="134" source="../images/tileset/Objects/Trees/5.png"/>
+ </tile>
+ <tile id="20">
+  <image width="84" height="155" source="../images/tileset/Objects/Trees/6.png"/>
+ </tile>
+ <tile id="21">
+  <image width="109" height="160" source="../images/tileset/Objects/Trees/1.png"/>
+ </tile>
+ <tile id="22">
+  <image width="123" height="158" source="../images/tileset/Objects/Trees/2.png"/>
+ </tile>
+ <tile id="23">
+  <image width="123" height="150" source="../images/tileset/Objects/Trees/3.png"/>
+ </tile>
+ <tile id="24">
+  <image width="150" height="160" source="../images/tileset/Objects/Trees/4.png"/>
+ </tile>
+ <tile id="25">
+  <image width="14" height="14" source="../images/tileset/Objects/Grass/6.png"/>
+ </tile>
+ <tile id="26">
+  <image width="14" height="14" source="../images/tileset/Objects/Grass/1.png"/>
+ </tile>
+ <tile id="27">
+  <image width="14" height="14" source="../images/tileset/Objects/Grass/2.png"/>
+ </tile>
+ <tile id="28">
+  <image width="14" height="14" source="../images/tileset/Objects/Grass/3.png"/>
+ </tile>
+ <tile id="29">
+  <image width="14" height="14" source="../images/tileset/Objects/Grass/4.png"/>
+ </tile>
+ <tile id="30">
+  <image width="14" height="14" source="../images/tileset/Objects/Grass/5.png"/>
+ </tile>
+</tileset>
