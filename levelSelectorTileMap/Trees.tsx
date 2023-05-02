9bc6ac67cbdc1abe4e050522a0d18d650e07120d
handasahil <113319786+handasahil@users.noreply.github.com>
2023-05-02 13:33:58 -0400

    level selector map assets and edits

--- levelSelectorTileMap/Trees.tsx
+++ levelSelectorTileMap/Trees.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Trees" tilewidth="28" tileheight="44" tilecount="9" columns="0">
+<tileset version="1.9" tiledversion="1.9.2" name="Trees" tilewidth="39" tileheight="48" tilecount="24" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="27" height="44" source="setOne/2 Objects/Trees/1.png"/>
@@ -27,5 +27,50 @@
  </tile>
  <tile id="8">
   <image width="10" height="43" source="setOne/2 Objects/Trees/9.png"/>
+ </tile>
+ <tile id="9">
+  <image width="39" height="43" source="setTwo/2 Objects/Trees/1.png"/>
+ </tile>
+ <tile id="10">
+  <image width="31" height="37" source="setTwo/2 Objects/Trees/2.png"/>
+ </tile>
+ <tile id="11">
+  <image width="29" height="41" source="setTwo/2 Objects/Trees/3.png"/>
+ </tile>
+ <tile id="12">
+  <image width="24" height="16" source="setTwo/2 Objects/Trees/4.png"/>
+ </tile>
+ <tile id="13">
+  <image width="19" height="14" source="setTwo/2 Objects/Trees/5.png"/>
+ </tile>
+ <tile id="14">
+  <image width="14" height="9" source="setTwo/2 Objects/Trees/6.png"/>
+ </tile>
+ <tile id="15">
+  <image width="16" height="27" source="setTwo/2 Objects/Trees/7.png"/>
+ </tile>
+ <tile id="16">
+  <image width="17" height="23" source="setTwo/2 Objects/Trees/8.png"/>
+ </tile>
+ <tile id="17">
+  <image width="11" height="17" source="setTwo/2 Objects/Trees/9.png"/>
+ </tile>
+ <tile id="18">
+  <image width="39" height="37" source="setTwo/2 Objects/Trees/10.png"/>
+ </tile>
+ <tile id="19">
+  <image width="27" height="37" source="setTwo/2 Objects/Trees/11.png"/>
+ </tile>
+ <tile id="20">
+  <image width="31" height="45" source="setTwo/2 Objects/Trees/12.png"/>
+ </tile>
+ <tile id="21">
+  <image width="29" height="48" source="setTwo/2 Objects/Trees/13.png"/>
+ </tile>
+ <tile id="22">
+  <image width="19" height="35" source="setTwo/2 Objects/Trees/14.png"/>
+ </tile>
+ <tile id="23">
+  <image width="13" height="25" source="setTwo/2 Objects/Trees/15.png"/>
  </tile>
 </tileset>
---
+++ levelSelectorTileMap/houses.tsx
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="houses" tilewidth="46" tileheight="90" tilecount="15" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="38" source="setOne/2 Objects/Houses/1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="33" height="38" source="setOne/2 Objects/Houses/2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="43" height="38" source="setOne/2 Objects/Houses/3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="35" height="63" source="setOne/2 Objects/Houses/4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="43" height="40" source="setOne/2 Objects/Houses/5.png"/>
+ </tile>
+ <tile id="5">
+  <image width="27" height="26" source="setOne/2 Objects/Houses/6.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="setTwo/2 Objects/Houses/1.png"/>
+ </tile>
+ <tile id="7">
+  <image width="35" height="27" source="setTwo/2 Objects/Houses/2.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="27" source="setTwo/2 Objects/Houses/3.png"/>
+ </tile>
+ <tile id="9">
+  <image width="38" height="32" source="setTwo/2 Objects/Houses/4.png"/>
+ </tile>
+ <tile id="10">
+  <image width="38" height="32" source="setTwo/2 Objects/Houses/5.png"/>
+ </tile>
+ <tile id="11">
+  <image width="44" height="42" source="setTwo/2 Objects/Houses/6.png"/>
+ </tile>
+ <tile id="12">
+  <image width="46" height="60" source="setTwo/2 Objects/Houses/7.png"/>
+ </tile>
+ <tile id="13">
+  <image width="46" height="88" source="setTwo/2 Objects/Houses/8.png"/>
+ </tile>
+ <tile id="14">
+  <image width="46" height="90" source="setTwo/2 Objects/Houses/9.png"/>
+ </tile>
+</tileset>
