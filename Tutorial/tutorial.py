812389d999e900d598e3412f3eadf56de97cc818
EL132 <elias.lind6@gmail.com>
2023-05-08 13:15:43 -0400

    tutorial

--- Tutorial/tutorial.py
+++ Tutorial/tutorial.py
@@ -34,6 +34,7 @@ class Tutorial():
     def __init__(self, level):
         self.level = level
         self.custom_font = pygame.font.Font('./Levels/LevelOne/fonts/ARCADECLASSIC.ttf', 32)
+        self.small_font = pygame.font.Font('./Levels/LevelOne/fonts/ARCADECLASSIC.ttf', 24)
 
         self.player_group = pygame.sprite.Group()
 
@@ -42,13 +43,15 @@ class Tutorial():
         elif self.level == 2:
             self.player = playerTwo(100, 100, land_sprite_group)
         else:
-            self.player = 0
+            self.player = playerTwo(100, 100, land_sprite_group)
 
         self.player_group.add(self.player)
         self.started = False
         
         self.starting_time = time.time()
         self.time_passed = 0
+
+        self.beginning_text = False
 
     def pause_game(self, main_text, sub_text1, sub_text2):
         time_initial = time.time()
@@ -83,7 +86,7 @@ class Tutorial():
 
         #Display the pause text
         # screen.fill(BLACK)
-        pygame.draw.rect(screen, BLACK, pygame.Rect(150, 80, 475, 180), 3)
+        pygame.draw.rect(screen, WHITE, pygame.Rect(150, 80, 475, 180), 3)
         pygame.draw.line(screen, WHITE, (153, 150), (621, 150), 3)
         screen.blit(main_text, main_rect)
         screen.blit(sub_text1, sub_rect1)
@@ -99,25 +102,28 @@ class Tutorial():
                 if event.type == pygame.KEYDOWN:
                     #User wants to continue
                     if event.key == pygame.K_RETURN:
-                        time_passed = time.time() - time_initial
-                        self.display_time = int(self.display_time)
-                        self.display_time -= time_passed
                         is_paused = False
-                        self.starting_time = time.time()
-                        # pygame.mixer.music.unpause()
-                #User wants to quit
                 if event.type == pygame.QUIT:
                     is_paused = False
                     pygame.quit()
 
+    def blurSurf(self, surface, amt):
+        if amt < 1.0:
+            raise ValueError("Arg 'amt' must be greater than 1.0, passed in value is %s"%amt)
+        scale = 1.0/float(amt)
+        surf_size = surface.get_size()
+        scale_size = (int(surf_size[0]*scale), int(surf_size[1]*scale))
+        surf = pygame.transform.smoothscale(surface, scale_size)
+        surf = pygame.transform.smoothscale(surf, surf_size)
+        return surf
+
     def starting_text(self):
         if self.started:
-            beginning_text = True
+            self.beginning_text = True
             # video : 
-            messages = ['Welcome to the tutorial!', 
-                        'You will be learning how to play the game.',
-                        'You will get access to different abilities as you complete more levels.',
-                        'Come back later to learn how to use them as you play.']
+            messages = ['Welcome    to    the    tutorial!', 
+                'You    get    different    abilities    as    you    play',
+                'Come    back    later    to    test    them    out!']
             snip = self.custom_font.render('', True, (255, 255, 255))
             counter = 0
             # the bigger the speed variable, the slower it goes because of math
@@ -127,21 +133,23 @@ class Tutorial():
             done = False
             
 
-            while beginning_text:
-                if counter < speed * len(message):
-                    counter += 1
-                elif counter >= speed * len(message):
-                    done = True
+            while self.beginning_text:
                 for event in pygame.event.get():
                     if event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_RETURN and done and active_message < len(messages) - 1:
-                            if active_message == 3:
-                                beginning_text = False
+                        if event.key == pygame.K_RETURN and done and active_message < len(messages):
+                            if active_message == 2:
+                                self.beginning_text = False
+                                break
                             active_message += 1
                             done = False
                             message = messages[active_message]
                             counter = 0
                             self.update()
+                if counter < speed * len(message):
+                    counter += 1
+                elif counter >= speed * len(message):
+                    done = True
+                
 
 
                 
@@ -149,6 +157,41 @@ class Tutorial():
                 screen.blit(snip, (50, 100))
 
                 pygame.display.flip()
+
+    def key(self):
+        key_binds = ['LEFT', 'RIGHT', 'UP', 'SPACE', 'RSHIFT', 'ESCAPE', 'Q', 'W', 'R', 'T', 'Y', 'SPACE']
+        directions = ['move left', 'move right', 'jump', 'jump', 'sprint', 'pause', 'attack 1', 'attack 2', 'roll', 'taunt', 'emote', 'double jump']
+
+        # i want to draw a rectangle for each key bind
+        for i in range(0, 8):
+            if not 15 + 50 * i > 200:
+                pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(25, 15 + 50 * i, 125, 50), 3)
+                snip = self.custom_font.render(key_binds[i], True, (255, 255, 255))
+                screen.blit(snip, (35, 25 + 50 * i))
+                direction = self.small_font.render(directions[i], True, (255, 255, 255))
+                screen.blit(direction, (160, 25 + 50 * i))
+            else:
+                pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(290, 15 + 50 * (i - 4), 125, 50), 3)
+                snip = self.custom_font.render(key_binds[i], True, (255, 255, 255))
+                screen.blit(snip, (300, 25 + 50 * (i - 4)))
+                direction = self.small_font.render(directions[i], True, (255, 255, 255))
+                screen.blit(direction, (425, 25 + 50 * (i - 4)))
+        
+        if self.level >= 2:
+            for i in range(0, 3):
+                pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(530, 15 + 50 * i, 125, 50), 3)
+                snip = self.custom_font.render(key_binds[i + 8], True, (255, 255, 255))
+                screen.blit(snip, (540, 25 + 50 * i))
+                direction = self.small_font.render(directions[i + 8], True, (255, 255, 255))
+                screen.blit(direction, (665, 25 + 50 * i))
+        
+        if self.level >= 3:
+            pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(530, 15 + 150, 125, 50), 3)
+            snip = self.custom_font.render(key_binds[11], True, (255, 255, 255))
+            screen.blit(snip, (540, 25 + 150))
+            direction = self.small_font.render(directions[11], True, (255, 255, 255))
+            screen.blit(direction, (665, 25 + 150))
+
 
     def update(self):
         sprite_group.update()
@@ -159,9 +202,13 @@ class Tutorial():
 
         self.time_passed = time.time() - self.starting_time
 
-        if self.time_passed > 3 and not self.started:
-            self.started = True
-            self.starting_text()
+        # if self.time_passed > 1 and not self.started:
+        #     self.started = True
+        #     self.starting_text()
+
+        # if not self.beginning_text and self.started:
+        #     self.key()
+        self.key()
 
 
 
