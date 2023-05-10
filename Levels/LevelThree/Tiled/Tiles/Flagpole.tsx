7a59ebc7446691f1a71118122f5ffd83fb85ed94
EL132 <elias.lind6@gmail.com>
2023-05-10 11:06:19 -0400

    level three and (LSHIFT or RSHIFT)

---
+++ Levels/LevelThree/Tiled/Tiles/Flagpole.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Flagpole" tilewidth="30" tileheight="48" tilecount="1" columns="1">
+ <image source="../Objects/Flagpole.png" width="30" height="48"/>
+</tileset>
