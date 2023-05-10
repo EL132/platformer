8ade8ec265ffb3691ac94e19846189ea2dff2bcb
EL132 <elias.lind6@gmail.com>
2023-05-10 09:06:52 -0400

    level three map

---
+++ Levels/LevelThree/Tiled/Tiles/middle-transition.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="middle-transition" tilewidth="32" tileheight="32" tilecount="12" columns="12">
+ <image source="../Individual Edited Squares/middle-transition.png" width="384" height="32"/>
+</tileset>
